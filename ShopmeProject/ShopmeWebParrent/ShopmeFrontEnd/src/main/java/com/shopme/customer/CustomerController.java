518abb9a10041e76b90d3a3713dbbab9755e3db2
shahrukhk010 <khanshahrukhbsc@gmail.com>
2022-09-15 15:08:26 +0530

    working on useraccount updateion

--- ShopmeProject/ShopmeWebParrent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
+++ ShopmeProject/ShopmeWebParrent/ShopmeFrontEnd/src/main/java/com/shopme/customer/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.RememberMeAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -23,6 +24,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.shopme.Utility;
 import com.shopme.common.entity.Country;
 import com.shopme.common.entity.Customer;
+import com.shopme.security.CustomerUserDetails;
 import com.shopme.security.oauth.CustomerOauthUser;
 import com.shopme.setting.EmailSettingBag;
 import com.shopme.setting.SettingService;
@@ -146,7 +148,13 @@ public class CustomerController {
 		String fullName = customer.getFirstName() + " " + customer.getLastName();
 
 		if (principal instanceof UsernamePasswordAuthenticationToken
-				|| principal instanceof RememberMeAuthenticationToken) {}
+				|| principal instanceof RememberMeAuthenticationToken) {
+
+			CustomerUserDetails customerUserDetails = getCustomerUserDetailsObject(principal);
+			Customer authenticateCustomer = customerUserDetails.getCustomer();
+			authenticateCustomer.setFirstName(customer.getFirstName());
+			authenticateCustomer.setLastName(customer.getLastName());
+		}
 
 		else if (principal instanceof OAuth2AuthenticationToken) {
 
@@ -154,5 +162,22 @@ public class CustomerController {
 			CustomerOauthUser customOAuth = (CustomerOauthUser) oauth.getPrincipal();
 			customOAuth.setFullName(fullName);
 		}
+	}
+
+	private CustomerUserDetails getCustomerUserDetailsObject(Object principal) {
+
+		CustomerUserDetails userDetails = null;
+
+		if (principal instanceof UsernamePasswordAuthenticationToken) {
+
+			UsernamePasswordAuthenticationToken token = (UsernamePasswordAuthenticationToken) principal;
+			userDetails = (CustomerUserDetails) token.getPrincipal();
+
+		} else if (principal instanceof RememberMeAuthenticationToken) {
+			RememberMeAuthenticationToken token = (RememberMeAuthenticationToken) principal;
+			userDetails = (CustomerUserDetails) principal;
+		}
+
+		return userDetails;
 	}
 }
