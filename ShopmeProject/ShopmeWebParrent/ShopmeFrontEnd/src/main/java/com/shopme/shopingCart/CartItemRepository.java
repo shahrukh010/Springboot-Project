fa34895c2c474e5908b4612c6cf8ecf301b16acc
shahrukhk010 <khanshahrukhbsc@gmail.com>
2022-09-23 11:51:48 +0530

    working on shopingcart module

--- ShopmeProject/ShopmeWebParrent/ShopmeFrontEnd/src/main/java/com/shopme/shopingCart/CartItemRepository.java
+++ ShopmeProject/ShopmeWebParrent/ShopmeFrontEnd/src/main/java/com/shopme/shopingCart/CartItemRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.shopme.common.entity.CartItem;
 import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.Product;
 
 public interface CartItemRepository extends CrudRepository<CartItem, Integer> {
-	
-	
+
 	public List<CartItem> findByCustomer(Customer customer);
+
+	public CartItem findByCustomerAndProduct(Customer customer, Product product);
+
+	public void updateQuantity(int quantity, Product product, Customer customer);
 
 }
