7be9a9a236aef01029389930043760d013dc3381
shahrukhk010 <khanshahrukhbsc@gmail.com>
2022-09-24 19:09:56 +0530

    working on Shopingcart module

--- ShopmeProject/ShopmeWebParrent/ShopmeFrontEnd/src/main/java/com/shopme/shopingCart/ShopingCartService.java
+++ ShopmeProject/ShopmeWebParrent/ShopmeFrontEnd/src/main/java/com/shopme/shopingCart/ShopingCartService.java
@@ -1,5 +1,36 @@
 package com.shopme.shopingCart;
 
+import org.springframework.stereotype.Service;
+
+import com.shopme.common.entity.CartItem;
+import com.shopme.common.entity.Customer;
+import com.shopme.common.entity.Product;
+
+@Service
 public class ShopingCartService {
+
+	private CartItemRepository cartRepo;
+
+	public Integer addProduct(Integer productId, Integer quantity, Customer customer) {
+
+		Integer updateQuantity = quantity;
+
+		Product product = new Product(productId);
+		CartItem cartItem = cartRepo.findByCustomerAndProduct(customer, product);
+
+		if (cartItem != null) {
+
+			updateQuantity = cartItem.getQuantity() + updateQuantity;
+			cartItem.setQuantity(updateQuantity);
+		} else {
+
+			cartItem.setCustomer(customer);
+			cartItem.setProduct(product);
+		}
+		cartItem.setQuantity(updateQuantity);
+		cartRepo.save(cartItem);
+
+		return updateQuantity;
+	}
 
 }
